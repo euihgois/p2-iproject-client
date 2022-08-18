237be783cf0345a4430f6772b909297bd38a45ef
euihgois <iskand.agung@gmail.com>
2022-08-18 12:08:39 +0700

    undone

--- WNAI-Client/src/App.vue
+++ WNAI-Client/src/App.vue
@@ -1,3 +1,39 @@
-<script></script>
+<script>
+import axios from "./apis/axios";
 
-<template></template>
+export default {
+  data() {
+    return {
+      file: null,
+    };
+  },
+  methods: {
+    async onSelect(event) {
+      const file = event.target.files[0];
+      console.log(file);
+      this.file = file;
+    },
+    async send() {
+      try {
+        let formData = new FormData();
+        formData.append("file", this.file);
+        await axios.post("test", formData, {
+          headers: {
+            "Content-Type": "multipart/form-data",
+          },
+        });
+      } catch (error) {
+        console.log("erorrr");
+      }
+    },
+  },
+};
+</script>
+
+<template>
+  <h1>TEST</h1>
+  <form>
+    <input ref="file" type="file" @change="onSelect" />
+    <button @click.prevent="send">Upload</button>
+  </form>
+</template>
---
+++ WNAI-Client/src/components/FileUpload.vue
@@ -0,0 +1,19 @@
+<template>
+  <form novalidate @submit.prevent="onSubmit">
+    <file-upload v-model="form.file"></file-upload>
+  </form>
+</template>
+
+<script>
+import fileUpload from "fileUpload";
+export default {
+  components: { fileUpload },
+  data: () => ({ form: { file: null } }),
+  methods: {
+    onSubmit() {
+      console.log(this.form);
+      // post the form to the server
+    },
+  },
+};
+</script>
